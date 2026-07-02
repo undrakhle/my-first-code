cc7d9eaeaae103998aad1c1d51cc96238405791a
undrakhle <g.undrakh113@gmail.com>
2026-07-03 07:00:51 +0800

    undrakh 7/3/2026;07:00am

--- код.py
+++ код.py
@@ -1,1 +1,7 @@
-print("Bi NPC bish, bi Buteegch!") 
+name = input("Student name: ")
+age = input("Age: ")
+score = input("Score (0-100): ")
+
+print("\n--- Student Information ---")
+print("Name:", name)
+print("Years left until graduation:", 22 - int(age))
