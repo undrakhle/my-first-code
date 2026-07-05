f8706353d730bc7ab72c8b0457e3327fe78db987
undrakhle <g.undrakh113@gmail.com>
2026-07-05 16:22:51 +0800

    undrakh 7/5/2026; if/else logic nemsen

--- код.py
+++ код.py
@@ -1,7 +1,13 @@
-name = input("Student name: ")
-age = input("Age: ")
-score = input("Score (0-100): ")
+name = input("Оюутны нэр: ")
+age = input("Нас: ")
+score = input("Авсан дүн (0-100): ")
 
-print("\n--- Student Information ---")
-print("Name:", name)
-print("Years left until graduation:", 22 - int(age))
+print("\n--- Оюутны Мэдээлэл ---")
+print("Нэр:", name)
+print("Төгсөхөд үлдсэн нас:", 22 - int(age))
+
+
+if int(score) >= 60:
+    print("Шалгалтын үр дүн: ТЭНЦСЭН 🥳")
+else:
+    print("Шалгалтын үр дүн: УНАСАН 😢")
